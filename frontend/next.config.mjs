e3dc2b03d7383db568badebb53846dc05c56e557
iHaveahat <velayofamily@gmail.com>
2024-10-06 19:51:34 -0600

    aaaa

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,20 @@
+// next.config.mjs
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        // idk if this is gonn work
+        source: "/(.*)", // This matches all routes
+        headers: [
+          {
+            key: "Referrer-Policy",
+            value: "no-referrer-when-downgrade",
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
