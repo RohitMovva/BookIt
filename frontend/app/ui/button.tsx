a38b32c26fc56b28339b73a2c8e69ceed996339e
iHaveahat <velayofamily@gmail.com>
2024-10-12 20:43:43 -0600

    some button stuff

--- frontend/app/ui/button.tsx
+++ frontend/app/ui/button.tsx
@@ -10,9 +10,12 @@ interface ButtonProps {
   href?: string;
   type?: "button" | "submit" | "reset";
   img?: string;
+  size?: number;
+  imgSide?: "r" | "l";
   alt?: string;
   onClick?: () => void;
   isOpen?: boolean;
+  noAnimation?: boolean;
 }
 
 export default function Button({
@@ -24,22 +27,26 @@ export default function Button({
   href,
   type = "button",
   img = "",
+  size = 20,
+  imgSide = "l",
   alt = "",
   onClick,
   isOpen = false,
+  noAnimation = false,
 }: ButtonProps) {
   const buttonContent = (
     <button
       type={type}
       onClick={onClick}
-      className={`${bgColor} ${textColor} ${border} ${borderColor} line-clamp-1 flex h-12 transform items-center justify-center gap-x-2 rounded-xl px-4 transition-transform duration-300 hover:-translate-y-1`}
+      className={`${bgColor} ${textColor} ${border} ${borderColor} line-clamp-1 flex h-12 transform items-center justify-center gap-x-2 rounded-xl px-4 ${!noAnimation ? "transition-transform duration-300 hover:-translate-y-1" : ""}`}
     >
       {img && (
         <Image
           src={isOpen ? "/close.png" : img}
-          alt="alt"
-          width={20}
-          height={20}
+          alt={alt}
+          width={size}
+          height={size}
+          className={imgSide === "r" ? "order-last" : ""}
         />
       )}
       {text}
